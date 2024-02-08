92d8e072595eb86ebcd654322c43b1374d94ca67
ZIFENG278 <zifengzhang18@gmail.com>
2024-02-08 16:30:44 +0800

    feat: add image upscale

--- gr.py
+++ gr.py
@@ -3,52 +3,71 @@ import uuid
 import gradio as gr
 from upscale import Upscale, EngineOV
 
+model_list = ['realesrgan-x4_BF16_480.bmodel']
 
 
 # model_odd = load_model()
 # model_even = load_model()
 
-def run(input_path, audio_check, num_worker):
-    if not os.path.exists('./result'):
-        os.mkdir('./result')
-    output_path = './result/out_{}.mp4'.format(uuid.uuid4())
-    tmp_path = './result/temph264_{}.mp4'.format(uuid.uuid4())
+def run(input_path, model, type, num_worker=1, audio_check=None):
+    if not os.path.exists(os.path.join('./result', type)):
+        os.makedirs(os.path.join('./result', type), exist_ok=True)
+    if type == "video":
+        output_path = './result/video/out_{}.mp4'.format(uuid.uuid4())
+        tmp_path = './result/video/temph264_{}.mp4'.format(uuid.uuid4())
+    else:
+        output_path = './result/image/out_{}.jpg'.format(uuid.uuid4())
+        tmp_path = None
     # try:
-    up = Upscale(input_path, output_path, tmp_path, num_worker=num_worker)
+    up = Upscale(input_path, output_path, model, tmp_path, type, num_worker=num_worker)
     result_path = up(audio_check)
     # except Exception as e:
     #     return (e, None)
 
-    return ("Success upscale video, click download icon to download to local", result_path)
+    return ("Success upscale, click download icon to download to local", result_path)
 
 
 description = """
 # Real-ESRGAN with BM1684M🛸\n
-run Real-ESRGAN to upscale video resolution by TPU
+run Real-ESRGAN to upscale video/image resolution by TPU
 """
 
 if __name__ == '__main__':
     with gr.Blocks(analytics_enabled=False) as demo:
         with gr.Row():
             gr.Markdown(description)
+        with gr.Tab("Video"):
+            with gr.Row():
+                with gr.Column():
+                    up_video = gr.Video(format="mp4", label="upload a video to upscale resolution")
+                    with gr.Row():
+                        start_button = gr.Button("Start improve")
+                        audio_check = gr.Checkbox(
+                            label="audio",
+                            info="if click would output with audio"
+                        )
+                        num_worker = gr.Slider(1, 10, value=4, step=1, label="Thread", info="Choose between 1 and 10")
+                        model = gr.Dropdown(choices=model_list, value=model_list[0], info="select a model",
+                                            label="Model")
+                    info_text = gr.Textbox(label="info output", lines=3)
 
-        with gr.Row():
-            with gr.Column():
-                up_video = gr.Video(format="mp4", label="upload a video to upscale resolution")
-                with gr.Row():
-                    start_button = gr.Button("Start improve")
-                    audio_check = gr.Checkbox(
-                        label="audio",
-                        info="if click would output with audio"
-                    )
-                    num_worker = gr.Dropdown(choices=[1, 2, 3, 4, 5, 6], value=6, info="num of workers", label="Threads")
+                    ret_video = gr.Video(label="output video", format=None, autoplay=False)
+                    hide_textbox = gr.Textbox(value="video", visible=False)
+                    start_button.click(run, [up_video, model, hide_textbox, num_worker, audio_check], outputs=[info_text, ret_video])
 
-                info_text = gr.Textbox(label="info output", lines=3)
+        with gr.Tab("Image"):
+            with gr.Row():
+                with gr.Column():
+                    up_img = gr.Image(label="upload a image to upscale resolution", type="filepath")
+                    with gr.Row():
+                        start_button = gr.Button("Start improve")
+                        model = gr.Dropdown(choices=model_list, value=model_list[0], info="select a model",
+                                            label="Model")
+                    info_text = gr.Textbox(label="info output", lines=3)
 
-                ret_video = gr.Video(label="output video", format=None, autoplay=False)
-                start_button.click(run, [up_video, audio_check, num_worker], outputs=[info_text, ret_video])
+                    ret_img = gr.Image(label="upscale result")
+                    hide_textbox = gr.Textbox(value="image", visible=False)
+                    start_button.click(run, [up_img, model, hide_textbox], outputs=[info_text, ret_img])
 
     demo.queue(max_size=2)
     demo.launch(debug=True, show_api=True, share=False, server_name="0.0.0.0")
-
-
--- upscale.py
+++ upscale.py
@@ -10,12 +10,12 @@ import cv2
 del os.environ["LD_LIBRARY_PATH"]
 from threading import Thread
 import uuid
-MODEL_PATH = './model/realesrgan-x4_BF16_480.bmodel'
+MODEL_PATH = './model/'
 DEVICE_ID = 0
 
 
-def load_model():
-    model = EngineOV(model_path=MODEL_PATH, batch=1, device_id=DEVICE_ID)
+def load_model(model_name):
+    model = EngineOV(model_path=os.path.join(MODEL_PATH, model_name), batch=1, device_id=DEVICE_ID)
     return model
 
 class EngineOV:
@@ -43,7 +43,9 @@ class EngineOV:
         return results
 
 class Upscale():
-    def __init__(self, input_path, output_path, tmp_path=None, num_worker=1):
+    def __init__(self, input_path, output_path, model_name, tmp_path=None, type=None, num_worker=1):
+        self.model_name = model_name
+        self.type = type
         self.num_worker = int(num_worker)
         self.worker = []
         self.frames = []
@@ -60,7 +62,7 @@ class Upscale():
 
     def init_worker(self):
         for i in range(self.num_worker):
-            model = load_model()
+            model = load_model(self.model_name)
             self.worker.append(model)
             self.frames.append([])
 
@@ -71,9 +73,11 @@ class Upscale():
         self.inference_frames.clear()
         self.thread.clear()
 
-    def model_inference(self, model, frame, t_name, tqdm_tool):
+    def model_inference(self, model, frame, tqdm_tool):
         for i in frame:
-            res = model([np.array([i["data"]])])[0]
+            res = model([np.expand_dims(i['data'], axis=0)])[0]
+
+            # res = model([np.array([i["data"]])])[0]
             # 将图像从 chw 转换回 hwc
             frame_hwc = np.transpose(res, (0, 2, 3, 1))[0] * 255.0
             # 数据类型转回 uint8（为了写入到视频中）
@@ -84,107 +88,150 @@ class Upscale():
 
         # print(t_name + " inference finish")
 
+    def ratio_resize(self, img):
+        target_size = (480, 640)
+        old_size = img.shape[0:2]
+        ratio = min(float(target_size[i]) / (old_size[i]) for i in range(len(old_size)))
+        new_size = tuple([int(i * ratio) for i in old_size])
+        img = cv2.resize(img, new_size[::-1])
+        pad_w = target_size[1] - new_size[1]
+        pad_h = target_size[0] - new_size[0]
+        top, bottom = pad_h // 2, pad_h - (pad_h // 2)
+        left, right = pad_w // 2, pad_w - (pad_w // 2)
+        img_new = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, None, (0, 0, 0))
+        pad_black = (top, bottom, left, right)
+        return img_new, pad_black
+
 
     def __call__(self, audio_check):
-        self.cap = cv2.VideoCapture(self.input_path)
-        if not self.cap.isOpened():
-            print("Error: Couldn't open the video file.")
-            raise gr.Error("Couldn't open the video file.")
-
-        self.video_info['width'] = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.video_info['height'] = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        # print(self.video_info['width'])
-        # print(self.video_info['height'])
-        if self.video_info['width'] != 640 or self.video_info['height'] != 480:
-            self.cap.release()
-            self.input_path = resize_video(self.input_path)
+        if self.type == "video":
             self.cap = cv2.VideoCapture(self.input_path)
             if not self.cap.isOpened():
                 print("Error: Couldn't open the video file.")
                 raise gr.Error("Couldn't open the video file.")
+
             self.video_info['width'] = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             self.video_info['height'] = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            # print(self.video_info['width'])
+            # print(self.video_info['height'])
+            if self.video_info['width'] != 640 or self.video_info['height'] != 480:
+                self.cap.release()
+                self.input_path = resize_video(self.input_path)
+                self.cap = cv2.VideoCapture(self.input_path)
+                if not self.cap.isOpened():
+                    print("Error: Couldn't open the video file.")
+                    raise gr.Error("Couldn't open the video file.")
+                self.video_info['width'] = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                self.video_info['height'] = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        self.video_info['fps'] = self.cap.get(cv2.CAP_PROP_FPS)
-        self.video_info['all_frame_num'] = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            self.video_info['fps'] = self.cap.get(cv2.CAP_PROP_FPS)
+            self.video_info['all_frame_num'] = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')
-        self.video_writer = cv2.VideoWriter(self.tmp_path,
-                                            fourcc,
-                                            self.video_info['fps'],
-                                            (self.video_info['width'] * 4, self.video_info['height'] * 4))
+            fourcc = cv2.VideoWriter_fourcc(*'avc1')
+            self.video_writer = cv2.VideoWriter(self.tmp_path,
+                                                fourcc,
+                                                self.video_info['fps'],
+                                                (self.video_info['width'] * 4, self.video_info['height'] * 4))
 
-        SPLIT = 200
-        print(self.video_info)
-        total_task = self.video_info['all_frame_num'] // SPLIT + 1
-        left_frame = self.video_info['all_frame_num'] % SPLIT
+            SPLIT = 200
+            print(self.video_info)
+            total_task = self.video_info['all_frame_num'] // SPLIT + 1
+            left_frame = self.video_info['all_frame_num'] % SPLIT
 
-        each_task_frame = [SPLIT for _ in range(total_task - 1)]
-        each_task_frame.append(left_frame)
-        print("This would split {} tasks".format(total_task))
+            each_task_frame = [SPLIT for _ in range(total_task - 1)]
+            each_task_frame.append(left_frame)
+            print("This would split {} tasks".format(total_task))
 
-        for task_frame in each_task_frame:
+            for task_frame in each_task_frame:
+                self.clean_cache()
+                tqdm_tool = tqdm(total=task_frame)
+                start = time.time()
+                for i in range(task_frame):
+                # while True:
+                    ret, frame = self.cap.read()
+                    # 如果视频读取完毕，跳出循环
+                    if not ret:
+                        break
+                    # 数据类型转换为 float32
+                    frame = frame.astype(np.float32)
+                    # 将图像从 hwc 转换为 chw
+                    frame_chw = np.transpose(frame, (2, 0, 1))
+                    # 例如，这里你可以进行其他处理
+                    frame_chw = frame_chw / 255.0
+                    worker = i % self.num_worker
+                    self.frames[worker].append({"id": i, "data": frame_chw})
+                    tqdm_tool.update(1)
+                tqdm_tool.close()
+                print("frame ready")
+                print(str(round((time.time() - start) * 1000, 3)) + " ms")
+
+
+
+                tqdm_tool = tqdm(total=task_frame)
+                start = time.time()
+                for index, worker in enumerate(self.worker):
+                    t = Thread(target=self.model_inference, args=(worker, self.frames[index], tqdm_tool))
+                    self.thread.append(t)
+                for i in self.thread:
+                    i.start()
+
+                for i in self.thread:
+                    i.join()
+                tqdm_tool.close()
+                print(str(round((time.time() - start) * 1000, 3)) + " ms")
+
+
+
+                tqdm_tool = tqdm(total=task_frame)
+                start = time.time()
+                self.inference_frames = sorted(self.inference_frames, key=lambda x: x["id"])
+
+                for i in self.inference_frames:
+                    self.video_writer.write(i["data"])
+                    tqdm_tool.update(1)
+                    # self.tqdm.update(1)
+                # self.tqdm.close()
+                # 释放视频对象
+                tqdm_tool.close()
+                print(str(round((time.time() - start) * 1000, 3)) + " ms")
+
+            self.cap.release()
+            self.video_writer.release()
+            self.worker.clear()
+
+            if audio_check:
+                fuse_audio_with_ffmpeg(self.input_path, self.tmp_path, self.output_path)
+                return self.output_path
+            else:
+                return self.tmp_path
+
+
+        elif self.type == 'image':
+            img = cv2.imread(self.input_path)
+            h, w = img.shape[0:2]
+            pad_black = None
+            if h != 480 or w != 640:
+                img, pad_black = self.ratio_resize(img)
+
+            img = img.astype(np.float32)
+            # 将图像从 hwc 转换为 chw
+            frame_chw = np.transpose(img, (2, 0, 1))
+            # 例如，这里你可以进行其他处理
+            frame_chw = frame_chw / 255.0
+            self.frames[0].append({"id": 1, "data": frame_chw})
+            tqdm_tool = tqdm(total=1)
+            self.model_inference(self.worker[0], self.frames[0], tqdm_tool=tqdm_tool)
+            if pad_black:
+                self.inference_frames[0]['data'] = self.inference_frames[0]['data'][pad_black[0]*4:1920-pad_black[1]*4, pad_black[2]*4:2560-pad_black[3]*4, :]
+
+            cv2.imwrite(self.output_path, self.inference_frames[0]['data'])
             self.clean_cache()
-            tqdm_tool = tqdm(total=task_frame)
-            start = time.time()
-            for i in range(task_frame):
-            # while True:
-                ret, frame = self.cap.read()
-                # 如果视频读取完毕，跳出循环
-                if not ret:
-                    break
-                # 数据类型转换为 float32
-                frame = frame.astype(np.float32)
-                # 将图像从 hwc 转换为 chw
-                frame_chw = np.transpose(frame, (2, 0, 1))
-                # 例如，这里你可以进行其他处理
-                frame_chw = frame_chw / 255.0
-                worker = i % self.num_worker
-                self.frames[worker].append({"id": i, "data": frame_chw})
-                tqdm_tool.update(1)
-            tqdm_tool.close()
-            print("frame ready")
-            print(str(round((time.time() - start) * 1000, 3)) + " ms")
+            self.worker.clear()
 
 
-
-            tqdm_tool = tqdm(total=task_frame)
-            start = time.time()
-            for index, worker in enumerate(self.worker):
-                t = Thread(target=self.model_inference, args=(worker, self.frames[index], str(index), tqdm_tool))
-                self.thread.append(t)
-            for i in self.thread:
-                i.start()
-
-            for i in self.thread:
-                i.join()
-            tqdm_tool.close()
-            print(str(round((time.time() - start) * 1000, 3)) + " ms")
-
-
-
-            tqdm_tool = tqdm(total=task_frame)
-            start = time.time()
-            self.inference_frames = sorted(self.inference_frames, key=lambda x: x["id"])
-
-            for i in self.inference_frames:
-                self.video_writer.write(i["data"])
-                tqdm_tool.update(1)
-                # self.tqdm.update(1)
-            # self.tqdm.close()
-            # 释放视频对象
-            tqdm_tool.close()
-            print(str(round((time.time() - start) * 1000, 3)) + " ms")
-
-        self.cap.release()
-        self.video_writer.release()
-        self.worker.clear()
-
-        if audio_check:
-            fuse_audio_with_ffmpeg(self.input_path, self.tmp_path, self.output_path)
             return self.output_path
-        else:
-            return self.tmp_path
+
+
 
 
 
